8395440ae98eb7c3b064bf77539f1a289575994c
Sam Gearou <samgearou@gmail.com>
2015-11-20 13:39:41 -0800

    added files

--- src/binary_tree_io.c
+++ src/binary_tree_io.c
@@ -1,15 +1,16 @@
 // TODO: Implement the methods in binary_tree_io.c.
-#include <stdio.h>
+
 #include "binary_tree_io.h"
+#include <stdio.h>
 #include "binary_tree.h"
 
 
 void binary_tree_write(binary_tree* self, FILE* stream){
-return toFileHelper(self, stream);
+  toFileHelper(self, stream);
 }
 
 binary_tree* binary_tree_create_f(FILE* stream){
-return readFileHelper(stream);
+  return readFileHelper(stream);
 }
 
 binary_tree* readFileHelper(FILE* stream){
