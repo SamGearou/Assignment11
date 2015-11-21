9af52c7e25f43103a8dd62aba3bc9a2156b1c59a
Sam Gearou <samgearou@gmail.com>
2015-11-20 20:33:13 -0800

    more changes

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -6,11 +6,13 @@
 
 struct binary_tree
 {
-    char value[MAX_STRING_SIZE];
-    binary_tree* left;
-    binary_tree* right;
-    binary_tree* parent;
+  char value[MAX_STRING_SIZE];
+  binary_tree* left;
+  binary_tree* right;
+  binary_tree* parent;
 };
+int index=0;
+int *p = &index;
 
 binary_tree* binary_tree_create(){
   binary_tree* bTree = malloc(sizeof(binary_tree));
@@ -22,9 +24,97 @@ binary_tree* binary_tree_create(){
 
 binary_tree* binary_tree_create_s(char* str){
   binary_tree* bTree = malloc(sizeof(binary_tree));
-  bTree->value[0] = *str;
+  bTree->value[*p] = *str;
+  bTree->left = binary_tree_create();
+  bTree->right=binary_tree_create();
+  *p++;
+  return bTree;
 }
 
 binary_tree* binary_tree_create_stt(char* str, binary_tree* left, binary_tree* right){
+binary_tree* bTree = malloc(sizeof(binary_tree));
+bTree->value[*p] = *str;
+bTree->left = left;
+bTree->right = right;
+*p++;
+return bTree;
+}
+
+void binary_tree_destroy(binary_tree* self){
+
+}
+
+void binary_tree_set_left(binary_tree* self, binary_tree* left){//self = parent, check this method
+if(binary_tree_is_empty(self)){
+  return;
+}
+if(self->left != NULL){
+  self->left = left;
+}
+}
+
+void binary_tree_set_right(binary_tree* self, binary_tree* right){//self = parent, check this method
+if(binary_tree_is_empty(self)){
+  return;
+}
+if(self->right != NULL){
+  self->right = right;
+}
+}
+
+bool binary_tree_is_empty(binary_tree* self){
+ if(self->parent == NULL && self->left == NULL && self->right == NULL){
+   return true;
+ }
+ return false;
+}
+
+bool binary_tree_is_leaf(binary_tree* self){
+ if(self->parent != NULL && binary_tree_is_empty(self->left) && binary_tree_is_empty(self->right)){
+   return true;
+ }
+ return false;
+}
+
+bool binary_tree_is_left(binary_tree* self){
+
+}
+
+bool binary_tree_is_right(binary_tree* self){
+
+}
+
+bool binary_tree_is_root(binary_tree* self){
+if(self->parent == NULL && self->right != NULL && self->left != NULL){
+  return true;
+}
+return false;
+}
+
+int binary_tree_height(binary_tree* self){
+
+}
+
+int binary_tree_depth(binary_tree* self){
+
+}
+
+char* binary_tree_get_string(binary_tree* self, char* str){
+
+}
+
+binary_tree* binary_tree_get_left(binary_tree* self){
+  return self->left;
+}
+
+binary_tree* binary_tree_get_right(binary_tree* self){
+  return self->right;
+}
+
+binary_tree* binary_tree_get_parent(binary_tree* self){
+  return self->parent;
+}
+
+binary_tree* binary_tree_get_root(binary_tree* self){
 
 }
